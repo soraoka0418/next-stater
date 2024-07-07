86d6f827754a65b451d55e6c81f2b66c71f18dcd
okadasorachi <sorachi.okada@northdetail.co.jp>
2024-07-07 14:02:53 +0900

    fix: test環境のbug対応

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,11 @@
 import "@testing-library/jest-dom/vitest"
 import { cleanup } from "@testing-library/react"
-import { afterEach } from "vitest"
+import { afterEach, vi } from "vitest"
+
+// selectのclickイベントが発火しないための対応
+// 参考: https://github.com/testing-library/user-event/discussions/1087
+window.HTMLElement.prototype.scrollIntoView = vi.fn()
+window.HTMLElement.prototype.hasPointerCapture = vi.fn()
 
 afterEach(() => {
 	cleanup()
